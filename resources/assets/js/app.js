aebac57b93fc18bbaf7286efa55b3ecd45e4f6c7
Alex Crocker <croc122@gmail.com>
2017-06-29 13:18:05 -0400

    Configured dependencies

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,6 +8,7 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+window.VueRouter = require('vue-router');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
+        <link rel="stylesheet" href="{{ asset("css/font-awesome.min.css") }}">
         <!-- Styles -->
         <style>
             html, body {
