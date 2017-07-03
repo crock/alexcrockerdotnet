b7c48b0a87d2cc7e46c1169e6cd63d41838be9c7
Alex Crocker <croc122@gmail.com>
2017-07-03 12:21:10 -0400

    Added favicons and SEO tags

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,8 +5,17 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#5F9EA0">
+    <meta name="p:domain_verify" content="eb689c1d8f2ce5c7b4c1fbf704baa172"/>
+    <link rel="shortcut icon" href="{{ asset("favicon.ico") }}" type="image/x-icon"/>
+    <link rel="shortcut icon" href="{{ asset("favicon.png") }}" type="image/png"/>
 
     <title>Alex Crocker - @yield('title')</title>
+
+    <!-- SEO Stuff -->
+    <meta name="description" content="Full Stack Developer for hire">
+    <meta name="keywords" content="alex crocker, web developer, web designer, full stack, full stack developer, web dev, developer portfolio, vue.js website, laravel website">
+    <meta name="author" content="Alex Crocker">
 
     <!-- Fonts -->
     <link rel="stylesheet" href="{{ asset("css/font-awesome.min.css") }}" type="text/css">
